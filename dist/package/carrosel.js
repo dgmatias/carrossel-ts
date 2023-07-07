3a8baed2322821060849d89a0e9b9e102ec3961d
Diego Matias <dg.matias021@gmail.com>
2023-07-07 14:59:55 -0300

    adicionando o carrosel

--- dist/package/carrosel.js
+++ dist/package/carrosel.js
@@ -1,13 +1,35 @@
 var Carrosel = /** @class */ (function () {
     function Carrosel() {
-        this.imagens = [];
+        // buttonElement: HTMLButtonElement;
+        this.imagens = [
+            "url(static/img/img1.jpg)",
+            "url(static/img/img2.jpg)",
+            "url(static/img/img3.jpg)"
+        ];
+        this.indice = 0;
     }
+    // teste():void {
+    //     this.bodyElement = document.querySelector('body');
+    //     this.buttonElement = document.querySelector('button');
+    //     this.buttonElement.addEventListener("click", ()=>{
+    //         console.log("mudando imagem");
+    //         this.bodyElement.style.backgroundImage = "url(static/img/img3.jpg)";
+    //     })
+    // }
     Carrosel.prototype.mudarImagem = function () {
+        var _this = this;
+        this.bodyElement = document.querySelector('body');
         setInterval(function () {
             console.log("mudando imagem");
+            _this.indice++;
+            if (_this.indice === 3) {
+                _this.indice = 0;
+            }
+            _this.bodyElement.style.backgroundImage = _this.imagens[_this.indice];
         }, 5000);
     };
     return Carrosel;
 }());
 var carrosel = new Carrosel();
 carrosel.mudarImagem();
+// carrosel.teste();
--- src/package/carrosel.ts
+++ src/package/carrosel.ts
@@ -1,15 +1,41 @@
 class Carrosel {
 
-    body: HTMLBodyElement;
-    
-    imagens: string[] = [
+    bodyElement: HTMLBodyElement;
+    // buttonElement: HTMLButtonElement;
 
+    imagens: string[] = [
+        "url(static/img/img1.jpg)",
+        "url(static/img/img2.jpg)",
+        "url(static/img/img3.jpg)"
     ]
+
+    indice: number = 0;
+
+    // teste():void {
+    //     this.bodyElement = document.querySelector('body');
+    //     this.buttonElement = document.querySelector('button');
+        
+    //     this.buttonElement.addEventListener("click", ()=>{
+    //         console.log("mudando imagem");
+
+    //         this.bodyElement.style.backgroundImage = "url(static/img/img3.jpg)";
+    //     })
+
+    // }
 
     mudarImagem():void {
 
+        this.bodyElement = document.querySelector('body');
+
         setInterval(()=>{
             console.log("mudando imagem");
+            this.indice++
+
+            if(this.indice === 3) {
+                this.indice = 0;
+            }
+
+            this.bodyElement.style.backgroundImage = this.imagens[this.indice];
         }, 5000);
 
     }
@@ -18,3 +44,4 @@ class Carrosel {
 
 const carrosel = new Carrosel();
 carrosel.mudarImagem();
+// carrosel.teste();
